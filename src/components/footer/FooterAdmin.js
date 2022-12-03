dc1b0695cde3f13249b8da4a8d92427a488408c8
Miles Maltberts <milesmaltbie@gmail.com>
2022-12-02 18:51:48 -0800

    First commit

---
+++ src/components/footer/FooterAdmin.js
@@ -0,0 +1,26 @@
+/*eslint-disable*/
+import React from "react";
+import {
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Footer() {
+  const textColor = useColorModeValue("gray.400", "white");
+  return (
+    <Flex
+      zIndex='3'
+      flexDirection={{
+        base: "column",
+        xl: "row",
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start",
+      }}
+      justifyContent='space-between'
+      px={{ base: "30px", md: "50px" }}
+      pb='30px'>
+    </Flex>
+  );
+}
